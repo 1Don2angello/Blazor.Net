36fadf65a40eea7a1287600097b9b77656cdbc86
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-02-06 16:19:22 -0500

    update

--- BlazorPeliculas/BlazorPeliculas/Shared/obj/Debug/net6.0/BlazorPeliculas.Shared.AssemblyInfo.cs
+++ BlazorPeliculas/BlazorPeliculas/Shared/obj/Debug/net6.0/BlazorPeliculas.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbfbf398c554ae995ebe5563e99a4f3c24dfa5b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b4a5bdc32ebd2e93f0d384234ccfdaeddfb6cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
