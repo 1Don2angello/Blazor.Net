9550968dcd1aa73c2b2b725b7facad2ae9cdb796
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-05-21 16:16:06 -0500

    add proyecto web api

---
+++ IDYGS93-ASP-NET-CORE-WEB-API/IDYGS93-ASP-NET-CORE-WEB-API/Context/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace IDYGS93_ASP_NET_CORE_WEB_API.Context
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options):base (options)
+        { }
+
+        //Modelos a mapear 
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ IDYGS93-ASP-NET-CORE-WEB-API/IDYGS93-ASP-NET-CORE-WEB-API/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using IDYGS93_ASP_NET_CORE_WEB_API.Context;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
