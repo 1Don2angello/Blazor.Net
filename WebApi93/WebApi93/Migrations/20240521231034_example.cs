9b6f6263b1386be5818c1264c5fd6883d7978838
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-05-22 15:42:20 -0500

    actualizacion

---
+++ WebApi93/WebApi93/Migrations/20240521231034_example.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi93.Migrations
+{
+    public partial class example : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    PkRol = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.PkRol);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    PkUsuario = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FkRol = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.PkUsuario);
+                    table.ForeignKey(
+                        name: "FK_Usuarios_Roles_FkRol",
+                        column: x => x.FkRol,
+                        principalTable: "Roles",
+                        principalColumn: "PkRol");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "PkRol", "Nombre" },
+                values: new object[] { 1, "sa" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "PkUsuario", "FkRol", "Nombre", "Password", "User" },
+                values: new object[] { 1, 1, "Ana", "123", "Usuario" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_FkRol",
+                table: "Usuarios",
+                column: "FkRol");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
