776ee7f1d0d898bf83732f8fd5d4ced43d1ff662
1Don2angello <106407139+1Don2angello@users.noreply.github.com>
2024-05-22 16:50:13 -0500

    update

--- BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
+++ BlazorPeliculas/BlazorPeliculas/Server/obj/Debug/net7.0/BlazorPeliculas.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082154b3f374a46ced654d92c6d0600c3eec7f58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b6f6263b1386be5818c1264c5fd6883d7978838")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorPeliculas.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
